04b6e59e0f907dc6921d0e9564fd724ac09bd0af
Philip Zhang <zhangxilai@doublechaintech.com>
2020-06-06 23:53:33 +0800

    Fix style

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,8 +31,8 @@ class Header extends Component {
     render() {
         return (
             <div id="header">
-                <h1 className="title">优荣代建数据平台数据大屏</h1>
-                <span className="clock">{this.state.datetime}</span>
+                <h1 className="title" style={{fontSize:"60px"}}>优荣代建数据大屏</h1>
+                <span className="clock" style={{position:"absolute",top:0,right:0,fontSize:"60px"}}>{this.state.datetime}</span>
             </div>
         );
     }
